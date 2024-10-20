d7bd0120242e361ac457b57c2abcbbe942892849
tanishqkumar2003 <tanishqpayla@gmail.com>
2024-10-20 09:52:02 +0530

    Signup Route Added

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,28 +1,57 @@
-import { Hono } from 'hono'
+import { Hono } from "hono";
+import { PrismaClient } from "@prisma/client/edge";
+import { withAccelerate } from "@prisma/extension-accelerate";
+import { sign } from 'hono/jwt'
 
-const app = new Hono()
+const app = new Hono<{
+	Bindings: {
+		DATABASE_URL: string,
+    JWT_SECRET: string
+	}
+}>();
 
-app.post('/api/v1/signup', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post("/api/v1/signup", async (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
 
-app.post('/api/v1/signin', (c) => {
-  return c.text('Hello Hono!')
-})
+  const body = await c.req.json();
+  
+  const user = await prisma.user.create({
+    data:{
+      email: body.email,
+      password: body.password
+    }
+  })
 
-app.post('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
+  const payload = {
+    id : user.id
+    // exp: Math.floor(Date.now() / 1000) + 60 * 5, // Token expires in 5 minutes
+  }
+  const token = await sign(payload, c.env.JWT_SECRET)
 
-app.put('/api/v1/blog', (c) => {
-  return c.text('Hello Hono!')
-})
+  return c.json({
+    msg:"User succesfully created", 
+    token});
+});
 
-app.get('/api/v1/bllog:id', (c) => {
-  return c.text('Hello Hono!')
-})
+app.post("/api/v1/signin", (c) => {
+  return c.text("Hello Hono!");
+});
 
-export default app
+app.post("/api/v1/blog", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.put("/api/v1/blog", (c) => {
+  return c.text("Hello Hono!");
+});
+
+app.get("/api/v1/bllog:id", (c) => {
+  return c.text("Hello Hono!");
+});
+
+export default app;
 
 //DATABASE_URL="prisma://accelerate.prisma-data.net/?api_key=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcGlfa2V5IjoiYmNjZWZlZDItMGIxYy00NGMzLTgwN2YtM2M1YWUzNDhjY2FiIiwidGVuYW50X2lkIjoiNTA1MjczNThhOTI5MDY5ZWMwZWU5MTYxMDk1M2I5ZGQ1N2I4OWYxYjY4ZWI1ZWFiMjBhYjhjZWM2NWM0ODQ2NiIsImludGVybmFsX3NlY3JldCI6IjM1YjQ2ZGNlLWU0OTAtNGQ2ZS1iOWYxLTJkYjY5OTY4MTIwNyJ9.HlWkrxclB1QTO7JB-pViWXh8QEfg27yxEnUZ0uASXTs"
 //DIRECT_URL="<YOUR_DATABASE_CONNECTION_STRING>"
