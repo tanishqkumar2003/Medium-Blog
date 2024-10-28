62444ddd20c856f5a9a784e4983923f31ce26d4f
tanishqkumar2003 <tanishqpayla@gmail.com>
2024-10-28 17:42:55 +0530

    .

---
+++ frontend/src/pages/Blogs.tsx
@@ -0,0 +1,35 @@
+import { BlogCard } from "../components/BlogCard";
+
+export const Blogs = () => {
+  return (
+    <div>
+      <BlogCard
+        authorName={"harkiart"}
+        title={"blog title"}
+        content={
+          "blog content Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, nostrum esse asperiores perspiciatis ab molestiae blanditiis ad. Facere velit quam id esse debitis rerum, quasi ea reprehenderit fugit molestias! Tenetur.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, nostrum esse asperiores perspiciatis ab molestiae blanditiis ad. Facere velit quam id esse debitis rerum, quasi ea reprehenderit fugit molestias! Tenetur.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, nostrum esse asperiores perspiciatis ab molestiae blanditiis ad. Facere velit quam id esse debitis rerum, quasi ea reprehenderit fugit molestias! Tenetur."
+        }
+        publishedDate={"28 Oct 2024"}
+        imageUrl=""
+      />
+      <BlogCard
+        authorName={"harkiart"}
+        title={"blog title"}
+        content={
+          "blog content Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, nostrum esse asperiores perspiciatis ab molestiae blanditiis ad. Facere velit quam id esse debitis rerum, quasi ea reprehenderit fugit molestias! Tenetur.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, nostrum esse asperiores perspiciatis ab molestiae blanditiis ad. Facere velit quam id esse debitis rerum, quasi ea reprehenderit fugit molestias! Tenetur.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, nostrum esse asperiores perspiciatis ab molestiae blanditiis ad. Facere velit quam id esse debitis rerum, quasi ea reprehenderit fugit molestias! Tenetur."
+        }
+        publishedDate={"28 Oct 2024"}
+        imageUrl=""
+      />
+      <BlogCard
+        authorName={"harkiart"}
+        title={"blog title"}
+        content={
+          "blog content ctetur adipisicing elit. Ratione, nostrum esse asperiores perspiciatis ab molestiae blanditiis ad. Facere velit quam id esse debitis rerum, quasi ea reprehenderit fugit molestias! Tenetur.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, nostrum esse asperiores perspiciatis ab molestiae blanditiis ad. Facere velit quam id esse debitis rerum, quasi ea reprehenderit fugit molestias! Tenetur.Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione, nostrum esse asperiores perspiciatis ab molestiae blanditiis ad. Facere velit quam id esse debitis rerum, quasi ea reprehenderit fugit molestias! Tenetur."
+        }
+        publishedDate={"28 Oct 2024"}
+        imageUrl=""
+      />
+    </div>
+  );
+};
