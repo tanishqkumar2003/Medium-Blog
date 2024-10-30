002a1d43ba8ed1610fb765ec296c070faf33e5ed
tanishqkumar2003 <tanishqpayla@gmail.com>
2024-10-31 02:10:57 +0530

    Minor Updates

--- frontend/src/components/BlogDescription.tsx
+++ frontend/src/components/BlogDescription.tsx
@@ -9,8 +9,9 @@
 
 import { ChangeEvent } from "react"
 
-export const BlogDescription = ({onchange}: {onchange: (e:ChangeEvent<HTMLInputElement>) => void}) => {
+export const BlogDescription = ({onchange}: {onchange: (e:ChangeEvent<HTMLTextAreaElement>) => void}) => {
     return <div className="mb-6">
-    <input onChange={onchange} type="text" className="block p-2.5 w-full h-20 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."/>
+    {/* <input onChange={onchange} type="text" className="block p-2.5 w-full h-20 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."/> */}
+    <textarea onChange={onchange} className="block p-2.5 h-52 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Write your thoughts here..."></textarea>
 </div>
 }
